5cf016d031c92ab6efd5ada1124d845c0a4f4fd0
eli <eli.jones@gmail.com>
2014-05-01 17:08:59 -0400

    adding Destinations struct with Get(), Put() and Decay().  Tests included.

--- thebox.go
+++ thebox.go
@@ -1,5 +1,16 @@
 package thebox
 
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+type Signal struct {
+	payload interface{}
+	wait    chan bool
+}
+
 type Allotment struct {
 	Amount int // Some parcel of total value in cents.
 }
@@ -12,6 +23,19 @@ type Delta struct {
 type Destination struct {
 	Symbol string // "GOOG",  "GOOG1417Q525"
 	Type   string // "Stock", "Option"
+}
+
+type TimestampedDestination struct {
+	Destination Destination
+	Timestamp   int64 // Millisecond Timestamp.
+}
+
+type Destinations struct {
+	destinations []TimestampedDestination // Timestamped Destination so can decay.
+	maxage       int64                    // Oldest Destination allowed.  // Should be a function?  For "easy" tuning?
+	put          chan Signal              // New Destinations come down this channel.
+	decay        chan chan bool           // Process of decay has begun.
+	decaying     bool                     // Currently decaying, so block put,get.
 }
 
 type Trade struct {
@@ -32,13 +56,8 @@ type Money struct {
 	Allotments []Allotment         // Currently available Allotments.
 	Deltas     []Delta             // Current bits of Delta.
 	get        chan chan Allotment // Request allotment.
-	put        chan MoneySignal    // Put allotment.
+	put        chan Signal         // Put allotment.
 	reallot    chan chan bool      // Re-balance Allotments.
-}
-
-type MoneySignal struct {
-	payload Allotment
-	wait    chan bool
 }
 
 func (m *Money) Get() Allotment {
@@ -52,7 +71,7 @@ func (m *Money) Put(allotment Allotment, block bool) {
 	if block {
 		wait = make(chan bool)
 	}
-	m.put <- MoneySignal{payload: allotment, wait: wait}
+	m.put <- Signal{payload: allotment, wait: wait}
 	if block {
 		<-wait
 	}
@@ -75,7 +94,7 @@ func NewMoney(cash int) *Money {
 	m.Allotments = []Allotment{}
 	m.Deltas = []Delta{}
 	m.get = make(chan chan Allotment, 100)
-	m.put = make(chan MoneySignal, 100)
+	m.put = make(chan Signal, 100)
 	m.reallot = make(chan chan bool, 10)
 
 	// Process Get,Put, ReAllot calls.
@@ -85,16 +104,16 @@ func NewMoney(cash int) *Money {
 			select {
 			case c := <-m.get:
 				// Pop Allotment from m.Allotments and send it down c.
-
 				allotment, m.Allotments = m.Allotments[len(m.Allotments)-1], m.Allotments[:len(m.Allotments)-1]
 				m.Available -= allotment.Amount
 				c <- allotment
-			case moneysignal := <-m.put:
+			case signal := <-m.put:
 				// Push Allotment to m.Allotments.
-				m.Allotments = append(m.Allotments, moneysignal.payload)
-				m.Available += moneysignal.payload.Amount
-				if moneysignal.wait != nil {
-					moneysignal.wait <- true
+				allotment = signal.payload.(Allotment)
+				m.Allotments = append(m.Allotments, allotment)
+				m.Available += allotment.Amount
+				if signal.wait != nil {
+					signal.wait <- true
 				}
 			case wait := <-m.reallot:
 				m.Allotments = reallot(m.Available)
@@ -119,3 +138,83 @@ func reallot(cash int) []Allotment {
 	}
 	return allotments
 }
+
+func (d *Destinations) Get() (Destination, error) {
+	// Get random destination.  No locking since don't care? Maybe just a lock on len()?
+	// May want to block while decaying?  Or, should just structure Decay to only happen after any possible Get()-ing?
+	if len(d.destinations) == 0 {
+		return Destination{}, errors.New("No Destinations")
+	}
+	td := d.destinations[rand.Intn(len(d.destinations))]
+	return td.Destination, nil
+}
+
+func (d *Destinations) Put(destination Destination, block bool) {
+	// Put new destination.
+	var wait chan bool
+	if block {
+		wait = make(chan bool)
+	}
+	d.put <- Signal{payload: destination, wait: wait}
+	if block {
+		<-wait
+	}
+}
+
+func (d *Destinations) Decay() {
+	// Initiate Decay process.
+	wait := make(chan bool)
+	d.decay <- wait
+	<-wait
+}
+
+func NewDestinations(maxage int64) *Destinations {
+	d := &Destinations{}
+	d.destinations = []TimestampedDestination{}
+	d.maxage = maxage
+	d.put = make(chan Signal, 100)
+	d.decay = make(chan chan bool)
+	d.decaying = false
+
+	// Process Put, Decay calls.
+	go func() {
+		for {
+			select {
+			case signal := <-d.put:
+				// Push TimestampedDestination.
+				destination := signal.payload.(Destination)
+				d.destinations = append(d.destinations, TimestampedDestination{Destination: destination, Timestamp: MS(Now())})
+				if signal.wait != nil {
+					signal.wait <- true
+				}
+			case wait := <-d.decay:
+				d.decaying = true
+				// Do some decaying.
+				d.destinations = decay(d.destinations, d.maxage)
+				d.decaying = false
+				wait <- true
+			}
+		}
+	}()
+
+	return d
+}
+
+func decay(destinations []TimestampedDestination, maxage int64) []TimestampedDestination {
+	now := MS(Now())
+	newdestinations := []TimestampedDestination{}
+	for _, td := range destinations {
+		if td.Timestamp < now-maxage {
+			continue
+		}
+		newdestinations = append(newdestinations, td)
+	}
+	return newdestinations
+}
+
+// Utilities.
+var MS = func(time time.Time) int64 {
+	return time.UnixNano() / 1000000
+}
+
+var Now = func() time.Time { return time.Now() }
--- thebox_test.go
+++ thebox_test.go
@@ -2,6 +2,7 @@ package thebox
 
 import (
 	"testing"
+	"time"
 )
 
 func Test_Money(t *testing.T) {
@@ -24,5 +25,45 @@ func Test_Money(t *testing.T) {
 
 	if money.Available != money.Total {
 		t.Errorf("Available money should equal Total money! Available: %d", money.Available)
+	}
+}
+
+func Test_Destinations(t *testing.T) {
+	onedayinmilliseconds := int64(1 * 24 * 60 * 60 * 1000)
+	destinations := NewDestinations(onedayinmilliseconds)
+
+	if len(destinations.destinations) != 0 {
+		t.Errorf("Should be 0 destinations, but there are %d!", len(destinations.destinations))
+	}
+
+	destination, err := destinations.Get()
+	if err == nil {
+		t.Errorf("Should have received error, but got destination: %v, err: %s", destination, err)
+	}
+
+	destination = Destination{Symbol: "GOOG", Type: "stock"}
+	destinations.Put(destination, true)
+	if len(destinations.destinations) != 1 {
+		t.Errorf("Should be 1 destination, but there are %d!", len(destinations.destinations))
+	}
+
+	destination, err = destinations.Get()
+	if destination.Symbol != "GOOG" || destination.Type != "stock" {
+		t.Errorf("What have you done? Expected 'GOOG','stock' but received: %s, %s!", destination.Symbol, destination.Type)
+	}
+
+	now := MS(Now())
+	// Mock MS() to give time beyond maxage.
+	MS = func(time time.Time) int64 { return now + destinations.maxage + 10 }
+
+	destination = Destination{Symbol: "AAPL", Type: "stock"}
+	destinations.Put(destination, true)
+	if len(destinations.destinations) != 2 {
+		t.Errorf("Should be 2 destinations, but there are %d!", len(destinations.destinations))
+	}
+
+	destinations.Decay()
+	if len(destinations.destinations) != 1 {
+		t.Errorf("Should be 1 destination now, but there are %d!", len(destinations.destinations))
 	}
 }
