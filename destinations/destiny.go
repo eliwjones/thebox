8a8735bfbf71e570204834c3a3adef2be5d07803
eli <eli.jones@gmail.com>
2014-05-05 12:58:47 -0400

    destinations package now called 'destiny'. Creating distinction between Destination and Path.

---
+++ destinations/destiny.go
@@ -0,0 +1,103 @@
+package destiny
+
+import (
+	"errors"
+	"github.com/eliwjones/thebox/util"
+	"math/rand"
+)
+
+type Destination struct {
+	Underlying string // "GOOG",  "SPX",  // Underlying?
+	Symbol     string // function of Underlying? f(d.Underlying, 1, d.Type)
+	Type       string // "Stock", "Option", "Future"
+}
+
+type Path struct {
+	Destination Destination
+	LimitOpen   string // function of current (Bid, Ask)?  Too specific??
+	LimitClose  string // function of LimitOpen?
+	Timestamp   int64
+}
+
+type Destiny struct {
+	paths    []Path           // Timestamped Destination so can decay.
+	maxage   int64            // Oldest Destination allowed.  // Should be a function?  For "easy" tuning?
+	put      chan util.Signal // New Destinations come down this channel.
+	decay    chan chan bool   // Process of decay has begun.
+	decaying bool             // Currently decaying, so block put,get.
+}
+
+func (d *Destiny) Get() (Path, error) {
+	// Get random Path.  No locking since don't care? Maybe just a lock on len()?
+	// May want to block while decaying?  Or, should just structure Decay to only happen after any possible Get()-ing?
+	if len(d.paths) == 0 {
+		return Path{}, errors.New("No Paths to Destinations.")
+	}
+	path := d.paths[rand.Intn(len(d.paths))]
+	return path, nil
+}
+
+func (d *Destiny) Put(path Path, block bool) {
+	// Put new path.
+	var wait chan bool
+	if block {
+		wait = make(chan bool)
+	}
+	d.put <- util.Signal{Payload: path, Wait: wait}
+	if block {
+		<-wait
+	}
+}
+
+func (d *Destiny) Decay() {
+	// Initiate Decay process.
+	wait := make(chan bool)
+	d.decay <- wait
+	<-wait
+}
+
+func New(maxage int64) *Destiny {
+	d := &Destiny{}
+	d.paths = []Path{}
+	d.maxage = maxage
+	d.put = make(chan util.Signal, 100)
+	d.decay = make(chan chan bool)
+	d.decaying = false
+
+	// Process Put, Decay calls.
+	go func() {
+		for {
+			select {
+			case signal := <-d.put:
+				// Push Path.
+				path := signal.Payload.(Path)
+				if path != (Path{}) {
+					d.paths = append(d.paths, path)
+				}
+				if signal.Wait != nil {
+					signal.Wait <- true
+				}
+			case wait := <-d.decay:
+				d.decaying = true
+				// Do some decaying.
+				d.paths = decay(d.paths, d.maxage)
+				d.decaying = false
+				wait <- true
+			}
+		}
+	}()
+
+	return d
+}
+
+func decay(paths []Path, maxage int64) []Path {
+	now := util.MS(util.Now())
+	newpaths := []Path{}
+	for _, path := range paths {
+		if path.Timestamp < now-maxage {
+			continue
+		}
+		newpaths = append(newpaths, path)
+	}
+	return newpaths
+}
---
+++ destinations/destiny_test.go
@@ -0,0 +1,52 @@
+package destiny
+
+import (
+	"github.com/eliwjones/thebox/util"
+	"testing"
+)
+
+func Test_Destiny(t *testing.T) {
+	onedayinmilliseconds := int64(1 * 24 * 60 * 60 * 1000)
+	destiny := New(onedayinmilliseconds)
+
+	if len(destiny.paths) != 0 {
+		t.Errorf("Should be 0 destinations, but there are %d!", len(destiny.paths))
+	}
+
+	path, err := destiny.Get()
+	if err == nil {
+		t.Errorf("Should have received error, but got path: %v, err: %s", path, err)
+	}
+
+	destination := Destination{Symbol: "GOOG", Type: "stock"}
+	path = Path{Destination: destination, Timestamp: util.MS(util.Now())}
+	destiny.Put(path, true)
+	if len(destiny.paths) != 1 {
+		t.Errorf("Should be 1 path, but there are %d!", len(destiny.paths))
+	}
+
+	path, err = destiny.Get()
+	if path.Destination.Symbol != "GOOG" || path.Destination.Type != "stock" {
+		t.Errorf("What have you done? Expected 'GOOG','stock' but received: %s, %s!", destination.Symbol, destination.Type)
+	}
+
+	now := util.MS(util.Now())
+	destination = Destination{Symbol: "AAPL", Type: "stock"}
+	path = Path{Destination: destination, Timestamp: now - destiny.maxage - 10}
+	destiny.Put(path, true)
+	if len(destiny.paths) != 2 {
+		t.Errorf("Should be 2 paths, but there are %d!", len(destiny.paths))
+	}
+
+	destiny.Decay()
+	if len(destiny.paths) != 1 {
+		t.Errorf("Should be 1 path now, but there are %d!", len(destiny.paths))
+	}
+
+	// Verify cannot add empty path.
+	length := len(destiny.paths)
+	destiny.Put(Path{}, true)
+	if len(destiny.paths) != length {
+		t.Errorf("Empty Destination should not have been added!")
+	}
+}
