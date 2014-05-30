bad7a414ea458d21a1bf7c27e92e7c074d43dad2
eli <eli.jones@gmail.com>
2014-05-30 16:15:28 -0400

    Pulsar: moving towards more readable params. periods map indicates base period=1 and consequent speedups. Most likely, base period of 1 minute instead of 1 second.

--- pulsar/pulsar.go
+++ pulsar/pulsar.go
@@ -6,18 +6,29 @@ import (
 	"time"
 )
 
+var periods map[int]time.Duration = map[int]time.Duration{
+	1:     1 * time.Second,
+	1000:  1 * time.Millisecond,
+	5000:  200 * time.Microsecond, // Occassionally drops tics.
+	10000: 100 * time.Microsecond} // Seems to be max before dropping "too many" tics.
+
 type Pulsar struct {
+	now        int64                  // "Milliseconds" since Epoch.
+	period     time.Duration          // What is my current duration?
 	dispatcher *dispatcher.Dispatcher // Where to send 'pulses'
 }
 
-func New(period time.Duration) *Pulsar {
+func New(startms int64, speedup int) *Pulsar {
 	p := &Pulsar{}
+	p.now = startms
+	p.period = periods[speedup]
 	p.dispatcher = dispatcher.New(10)
-	ticker := time.NewTicker(period)
+	ticker := time.NewTicker(p.period)
 
 	go func() {
 		for _ = range ticker.C {
-			p.dispatcher.Send(true, "pulser")
+			p.dispatcher.Send(p.now, "pulser")
+			p.now += 1000
 		}
 
 	}()
--- pulsar/pulsar_test.go
+++ pulsar/pulsar_test.go
@@ -1,34 +1,41 @@
 package pulsar
 
 import (
+	"github.com/eliwjones/thebox/util/funcs"
+
 	"testing"
 	"time"
 )
 
 func Test_Pulsar_New(t *testing.T) {
-	p := New(1 * time.Millisecond)
+	p := New(funcs.MS(funcs.Now()), 1)
 
 	if p == nil {
 		t.Errorf("Something is broken badly.")
 	}
 }
 
-func Test_Pulsar_Period(t *testing.T) {
-	period := 1 * time.Millisecond
-	p := New(period)
+func Test_Pulsar_Periods(t *testing.T) {
+	for period := range periods {
+		p := New(0, period)
+		count := period/4
 
-	timer1 := make(chan interface{}, 200)
-	p.Subscribe("tester1", timer1)
+		timer1 := make(chan interface{}, 2*count)
+		p.Subscribe("tester1", timer1)
 
-	timer2 := make(chan interface{}, 200)
-	p.Subscribe("tester2", timer2)
+		timer2 := make(chan interface{}, 2*count)
+		p.Subscribe("tester2", timer2)
 
-	count := 100
-	time.Sleep(time.Duration(count) * period)
-	if len(timer1) != count {
-		t.Errorf("Expected: %d, Got: %d!", count, len(timer1))
-	}
-	if len(timer2) != count {
-		t.Errorf("Expected: %d, Got: %d!", count, len(timer2))
+		now := p.now
+		time.Sleep(time.Duration(count) * p.period)
+		if len(timer1) < count*98/100 {
+			t.Errorf("Period: %d - Expected: %d, Got: %d!", period, count, len(timer1))
+		}
+		if len(timer2) < count*98/100 {
+			t.Errorf("Period: %d - Expected: %d, Got: %d!", period, count, len(timer2))
+		}
+		if p.now < int64(count*1000*98/100)+now {
+			t.Errorf("Period: %d - Expected: %d, Got: %d! Diff: %d.", period, int64(count*1000)+now, p.now, (int64(count*1000)+now)-p.now)
+		}
 	}
 }
