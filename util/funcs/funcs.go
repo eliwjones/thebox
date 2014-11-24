46251b7ae69b42027090b4a87592762baa479f4e
eli <eli.jones@gmail.com>
2014-11-24 17:33:01 -0500

    util.funcs: adding DecodeOption() EncodeOption() functions..

--- util/funcs/funcs.go
+++ util/funcs/funcs.go
@@ -1,17 +1,64 @@
 package funcs
 
 import (
+	"github.com/eliwjones/thebox/util/structs"
+
 	"fmt"
 	"io/ioutil"
+	"reflect"
+	"strconv"
 	"strings"
 	"time"
 )
+
+var OptionEncodingOrder = []string{"Underlying", "Symbol", "Expiration", "Time", "Strike", "Bid", "Ask", "Last", "Volume", "OpenInterest", "IV", "Type"}
 
 var MS = func(time time.Time) int64 {
 	return time.UnixNano() / 1000000
 }
 
 var Now = func() time.Time { return time.Now() }
+
+func DecodeOption(eo string) (structs.Option, error) {
+	o := structs.Option{}
+	r := reflect.ValueOf(&o).Elem()
+
+	s := strings.Split(eo, ",")
+	for idx, v := range s {
+		f := r.FieldByName(OptionEncodingOrder[idx])
+		switch f.Type().Kind() {
+		case reflect.String:
+			f.SetString(v)
+		case reflect.Int:
+			val, _ := strconv.ParseInt(v, 10, 0)
+			f.SetInt(val)
+		case reflect.Float64:
+			val, _ := strconv.ParseFloat(v, 64)
+			f.SetFloat(v)
+
+		}
+	}
+
+	return o, nil
+}
+
+func EncodeOption(o structs.Option) (string, error) {
+	r := reflect.ValueOf(&o).Elem()
+	eo := ""
+	for _, propname := range OptionEncodingOrder {
+		v := r.FieldByName(propname)
+		switch v.Type().Kind() {
+		case reflect.String:
+			eo += "," + v.String()
+		case reflect.Int:
+			eo += "," + fmt.Sprintf("%d", v.Int())
+		case reflect.Float64:
+			eo += "," + fmt.Sprintf("%.5f", v.Float())
+
+		}
+	}
+	return eo[1:], nil
+}
 
 func GetConfig() (string, string, string, string, error) {
 	b, err := ioutil.ReadFile("config")
--- util/funcs/funcs_test.go
+++ util/funcs/funcs_test.go
@@ -1,8 +1,30 @@
 package funcs
 
 import (
+	"github.com/eliwjones/thebox/util/structs"
+
+	"reflect"
 	"testing"
 )
+
+func Test_OptionEncodingOrder(t *testing.T) {
+	o := structs.Option{}
+	r := reflect.ValueOf(&o).Elem()
+
+	propmap := map[string]bool{}
+	for _, propname := range OptionEncodingOrder {
+		propmap[propname] = true
+		if !r.FieldByName(propname).IsValid() {
+			t.Errorf("Invalid Propname: %s", propname)
+		}
+	}
+	if len(OptionEncodingOrder) < len(propmap) {
+		t.Errorf("Expected %d, Got: %d", len(OptionEncodingOrder), len(propmap))
+	}
+	if len(propmap) < r.NumField() {
+		t.Errorf("Expected %d, Got: %d", r.NumField(), len(propmap))
+	}
+}
 
 func Test_getConfig(t *testing.T) {
 	_, _, _, _, err := GetConfig()
