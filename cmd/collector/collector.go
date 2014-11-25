01d67336b60b2bdf4ce6ee5844cf924c6cf6bd37
eli <eli.jones@gmail.com>
2014-11-25 16:13:14 -0500

    collector: doing all data writing inside getData() goroutine.  Added hamfisted time blocks.  Recording stock info as well.  Sadly, must write individual chain quotes into large holder file.. since 64 byte file has to take up a 4k block.

--- cmd/collector/collector.go
+++ cmd/collector/collector.go
@@ -3,9 +3,7 @@ package main
 import (
 	"github.com/eliwjones/thebox/adapter/tdameritrade"
 	"github.com/eliwjones/thebox/util/funcs"
-	"github.com/eliwjones/thebox/util/structs"
 
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -24,50 +22,67 @@ func main() {
 		funcs.UpdateConfig(id, pass, sid, tda.JsessionID)
 	}
 
-	pipe := make(chan []structs.Option, len(symbols))
+	pipe := make(chan bool, len(symbols))
 	for _, symbol := range symbols {
 		fmt.Printf("Getting: %s\n", symbol)
-		go getOptions(tda, symbol, pipe)
+		go getData(tda, symbol, pipe)
 	}
 
-	holder := make([]structs.Option, 0)
 	for _, _ = range symbols {
-		options := <-pipe
-		if len(options) == 0 {
-			fmt.Println("Received empty options slice.")
+		result := <-pipe
+		if !result {
+			fmt.Println("Received err result.")
 			continue
 		}
-		fmt.Printf("Received: %s\n", options[0].Underlying)
-		holder = append(holder, options...)
-	}
-
-	now := time.Now()
-	limit := now.AddDate(0, 0, 22).Format("20060102")
-	fmt.Printf("LOCAL: %d:%d\n", now.Hour(), now.Minute())
-	fmt.Printf("UTC:   %d:%d\n", now.UTC().Hour(), now.UTC().Minute())
-
-	for _, option := range holder {
-		if option.Expiration > limit {
-			continue
-		}
-		stuff, err := json.Marshal(option)
-		if err != nil {
-			fmt.Sprintf("Got err: %s", err)
-		}
-		key := fmt.Sprintf("%s_%s_%s_%d", option.Underlying, option.Expiration, option.Type, option.Strike)
-		lazyWriteFile("data", key, stuff)
 	}
 }
 
-func getOptions(tda *tdameritrade.TDAmeritrade, symbol string, pipe chan []structs.Option) {
-	options, _, err := tda.GetOptions(symbol)
-	if err != nil {
-		fmt.Printf("Got err: %s\n", err)
+func getData(tda *tdameritrade.TDAmeritrade, symbol string, pipe chan bool) {
+	now := time.Now().UTC()
+	if now.Weekday() == time.Saturday || now.Weekday() == time.Sunday {
+		fmt.Println("No need for Sat, Sun.")
+		pipe <- false
 		return
 	}
-	// Write stock info here? or make frankenstein pipe to send both down?
-	// Make new pipe just for stock?
-	pipe <- options
+	early := "13:38"
+	late := "21:02"
+	tooEarly, _ := time.Parse("20060102 15:04", now.Format("20060102")+" "+early)
+	tooLate, _ := time.Parse("20060102 15:04", now.Format("20060102")+" "+late)
+	// Hamfisted block before 13:30 UTC and after 21:00 UTC.
+	if now.Before(tooEarly) || now.After(tooLate) {
+		fmt.Printf("Time %s is before %s UTC or after %s UTC\n", now.Format("15:04:05"), early, late)
+		pipe <- false
+		return
+	}
+
+	limit := now.AddDate(0, 0, 22).Format("20060102")
+
+	options, stock, err := tda.GetOptions(symbol)
+	if err != nil {
+		fmt.Printf("Got err: %s\n", err)
+		pipe <- false
+		return
+	}
+
+	es, _ := funcs.Encode(&stock, funcs.StockEncodingOrder)
+	key := fmt.Sprintf("%s", now.Format("20060102_150405"))
+	path := fmt.Sprintf("data/%s/s", stock.Symbol)
+	lazyWriteFile(path, key, []byte(es))
+
+	for _, option := range options {
+		if option.Expiration > limit {
+			continue
+		}
+		eo, err := funcs.Encode(&option, funcs.OptionEncodingOrder)
+		if err != nil {
+			fmt.Sprintf("Got err: %s", err)
+		}
+		key := fmt.Sprintf("%s_%d", now.Format("20060102_150405"), option.Strike)
+		path := fmt.Sprintf("data/%s/o/%s/%s", option.Underlying, option.Expiration, option.Type)
+		lazyWriteFile(path, key, []byte(eo))
+	}
+
+	pipe <- true
 }
 
 func lazyWriteFile(folderName string, fileName string, data []byte) error {
