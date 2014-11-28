1394feade7b42e78036dbe615f051cfbfcaa1285
eli <eli.jones@gmail.com>
2014-11-28 14:40:48 -0500

    collectord: now including symbols in config file.

--- cmd/collectord/collectord.go
+++ cmd/collectord/collectord.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	symbols  = [3]string{"AAPL", "GOOG", "BABA"}
+	symbols  = []string{}
 	root_dir = flag.String("root_dir", "", "Where to find config file, 'log' and 'data' directories?")
 )
 
@@ -30,6 +30,12 @@ func main() {
 	pass := lines[1]
 	sid := lines[2]
 	jsess := lines[3]
+	for _, symbol := range lines[4:] {
+		if symbol == "" {
+			continue
+		}
+		symbols = append(symbols, symbol)
+	}
 
 	tda := tdameritrade.New(id, pass, sid, jsess)
 	if tda.JsessionID != jsess {
