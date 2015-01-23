c0ae20784091f1974b737dfde660e9e0982a1bac
eli <eli.jones@gmail.com>
2015-01-23 14:20:44 -0500

    destiny: complete rewrite (still protean).  Receives timestamp pulses and populatese edges for current WeekID().  Filters edges by multiplier and underlying.

--- destiny/destiny.go
+++ destiny/destiny.go
@@ -1,181 +1,125 @@
 package destiny
 
 import (
-	"github.com/eliwjones/thebox/dispatcher"
+	"github.com/eliwjones/thebox/collector"
 	"github.com/eliwjones/thebox/util/funcs"
 	"github.com/eliwjones/thebox/util/structs"
 
-	"errors"
-	"math"
-	"math/rand"
+	"bytes"
+	"fmt"
+	"io/ioutil"
 )
 
 type Destiny struct {
-	destinations []structs.Destination         // Need those destinations.
-	paths        []structs.Path                // Timestamped paths to destinations..
-	maxage       int64                         // Oldest Path allowed.  // Should be a function?  For "easy" tuning?
-	put          chan structs.Signal           // New Paths come down this channel.
-	decay        chan chan bool                // Process of decay has begun.
-	decaying     bool                          // Currently decaying, so block put,get.
-	dispatcher   *dispatcher.Dispatcher        // My megaphone.
-	amIn         chan structs.AllotmentMessage // Allotments come in here.
-	dIn          chan structs.Delta            // Deltas come in here.
+	collector      *collector.Collector
+	dataDir        string // top level dir for data.
+	edges          map[int64][]structs.Maximum
+	edgeMultiplier int
+	id             string     // allows for namespacing and multiple simulation runs.
+	pulses         chan int64 // timestamps from pulsar come here.
+	underlying     string
+	weeksBack      int
 }
 
-func (d *Destiny) Get() (structs.Path, error) {
-	// Get random Path.  No locking since don't care? Maybe just a lock on len()?
-	// May want to block while decaying?  Or, should just structure Decay to only happen after any possible Get()-ing?
-	if len(d.paths) == 0 {
-		return structs.Path{}, errors.New("No Paths to Destinations.")
-	}
-	path := d.paths[rand.Intn(len(d.paths))]
-	return path, nil
-}
+func New(collector *collector.Collector, dataDir string, edgeMultiplier int, id string, underlying string, weeksBack int) *Destiny {
+	d := &Destiny{id: id, dataDir: dataDir, edgeMultiplier: edgeMultiplier,
+		underlying: underlying, weeksBack: weeksBack}
+	d.edges = map[int64][]structs.Maximum{}
+	d.pulses = make(chan int64, 1000)
 
-func (d *Destiny) Put(path structs.Path, block bool) {
-	// Put new path.
-	var wait chan bool
-	if block {
-		wait = make(chan bool)
-	}
-	d.put <- structs.Signal{Payload: path, Wait: wait}
-	if block {
-		<-wait
-	}
-}
-
-func (d *Destiny) Decay() {
-	// Initiate Decay process.
-	wait := make(chan bool)
-	d.decay <- wait
-	<-wait
-}
-
-func New(maxage int64) *Destiny {
-	d := &Destiny{}
-	d.paths = []structs.Path{}
-	d.maxage = maxage
-	d.put = make(chan structs.Signal, 100)
-	d.decay = make(chan chan bool)
-	d.decaying = false
-
-	d.dispatcher = dispatcher.New(1000)
-	d.amIn = make(chan structs.AllotmentMessage, 1000)
-	d.dIn = make(chan structs.Delta, 1000)
-
-	// Process Put, Decay calls.
-	go func() {
-		for {
-			select {
-			case signal := <-d.put:
-				// Push Path.
-				path := signal.Payload.(structs.Path)
-				if path != (structs.Path{}) {
-					d.paths = append(d.paths, path)
-				}
-				if signal.Wait != nil {
-					signal.Wait <- true
-				}
-			case wait := <-d.decay:
-				d.decaying = true
-				// Do some decaying.
-				d.paths = decay(d.paths, d.maxage)
-				d.decaying = false
-				wait <- true
-			}
-		}
-	}()
-
-	// Grind Allotments into ProtoOrders.
-	go func() {
-		for am := range d.amIn {
-			// Combine allotment with Path and send to Trader as ProtoOrder.
-			p, err := d.Get()
-			if err != nil {
-				if am.Reply != nil {
-					am.Reply <- am.Allotment
-				}
-				continue
-			}
-
-			po := structs.ProtoOrder{Allotment: am.Allotment, Path: p}
-			d.dispatcher.Send(po, "protoorder")
-
-			if am.Reply != nil {
-				am.Reply <- true
-			}
-		}
-	}()
-
-	// Process Deltas.
-	go func() {
-		for delta := range d.dIn {
-			// Dummy hueristic, use better.
-			pathModifier(delta, d)
-		}
-	}()
+	go d.processPulses()
 
 	return d
 }
 
-func decay(paths []structs.Path, maxage int64) []structs.Path {
-	now := funcs.MS(funcs.Now())
-	newpaths := []structs.Path{}
-	for _, path := range paths {
-		if path.Timestamp < now-maxage {
+func (d *Destiny) processPulses() {
+	currentWeekID := int64(0)
+	for timestamp := range d.pulses {
+		// if Change week, load new edges.
+		weekID := funcs.WeekID(timestamp)
+		if currentWeekID != weekID {
+			d.populateEdges(timestamp)
+			currentWeekID = weekID
+		}
+
+		//Grind into ProtoOrders to send to Trader.
+		for _, edge := range d.edges[funcs.TimestampID(timestamp)] {
+			// Stuff this edge somewhere?
+			fmt.Println(edge)
+		}
+	}
+}
+
+func (d *Destiny) populateEdges(timestamp int64) {
+	edges := []structs.Maximum{}
+
+	filename := fmt.Sprintf("%d", funcs.WeekID(timestamp))
+	path := fmt.Sprintf("%s/edges/%d", d.dataDir, d.weeksBack)
+	edgeData, err := ioutil.ReadFile(path + "/" + filename)
+	if err != nil {
+		// Not found, load from collector and persist.
+		edges = d.collector.GetPastNEdges(timestamp, d.weeksBack)
+
+		// Encode and save for future reference.
+		encodedEdges := ""
+		for _, edge := range edges {
+			e, err := funcs.Encode(&edge, funcs.MaximumEncodingOrder)
+			if err != nil {
+				fmt.Println("[populateEdges] Err encoding edge: %s", err)
+				continue
+			}
+			encodedEdges += e + "\n"
+		}
+		err = funcs.LazyWriteFile(path, filename, []byte(encodedEdges))
+		if err != nil {
+			message := fmt.Sprintf("Failed to write encodedEdges. Err: %s", err)
+			panic(message)
+		}
+	} else {
+		// Got edgeData, decode into []structs.Maximum
+		for _, edge := range bytes.Split(edgeData, []byte("\n")) {
+			e := structs.Maximum{}
+			err := funcs.Decode(string(edge), &e, funcs.MaximumEncodingOrder)
+			if err != nil {
+				panic("What to do?  Thought I had edges, but they buggy.  Just get from collector?")
+			}
+			edges = append(edges, e)
+		}
+	}
+
+	// Do filtering here.
+	edges = filterEdgesByUnderlying(edges, d.underlying)
+	edges = filterEdgesByMultiplier(edges, d.edgeMultiplier)
+
+	// Choose 30 Edges.
+	bag := funcs.ChooseMFromN(30, len(edges))
+	d.edges = map[int64][]structs.Maximum{}
+	for _, index := range bag {
+		edge := edges[index]
+		timestampID := funcs.TimestampID(edge.Timestamp)
+		d.edges[timestampID] = append(d.edges[timestampID], edge)
+	}
+}
+
+func filterEdgesByMultiplier(edges []structs.Maximum, multiplier int) []structs.Maximum {
+	filteredEdges := []structs.Maximum{}
+	for _, edge := range edges {
+		edgeMultiple := float64(edge.MaximumBid) / (float64(edge.OptionAsk) + float64(2.2))
+		if edgeMultiple < float64(multiplier) {
 			continue
 		}
-		newpaths = append(newpaths, path)
+		filteredEdges = append(filteredEdges, edge)
 	}
-	return newpaths
+	return filteredEdges
 }
 
-func pathModifier(delta structs.Delta, d *Destiny) {
-	if delta.Percent > 100 {
-		d.Put(delta.Path, false)
+func filterEdgesByUnderlying(edges []structs.Maximum, underlying string) []structs.Maximum {
+	filteredEdges := []structs.Maximum{}
+	for _, edge := range edges {
+		if edge.Underlying != underlying {
+			continue
+		}
+		filteredEdges = append(filteredEdges, edge)
 	}
+	return filteredEdges
 }
-
-// Protean Penalty Functions.
-
-func distanceFromStrikePenalty(strike int, maxStrike, underlyingPrice int, k float64) float64 {
-	// Not sure of least messy way to write out function.
-
-	strikeDistance := math.Abs(float64(strike - underlyingPrice))
-	maxDistance := math.Abs(float64(maxStrike - underlyingPrice))
-
-	penalty := k * (strikeDistance / maxDistance)
-
-	return penalty
-}
-
-func distanceFromExpirationPenalty(utcTimestamp int64, expirationTimestamp int64, k float64) float64 {
-	secondsInDay := int64(24 * 60 * 60)
-	maxDistance := float64(4 * secondsInDay)
-
-	currentDistance := float64(utcTimestamp - (expirationTimestamp - secondsInDay))
-	if currentDistance > 0 {
-		// We are on expiration day, no penalty for nothing.
-		return 0
-	}
-	currentDistance = math.Abs(currentDistance)
-
-	penalty := k * (currentDistance / maxDistance)
-
-	return penalty
-}
-
-func premiumPenalty(optionPrice int, closestPrice int, farthestPrice int, k float64) float64 {
-	currentDistance := math.Abs(float64(optionPrice - farthestPrice))
-	maxDistance := math.Abs(float64(closestPrice - farthestPrice))
-
-	penalty := k * (currentDistance / maxDistance)
-
-	return penalty
-}
-
-// Probability of adding option to list of paths?
-//     (1-distanceFromStrikePenalty() + 1-distanceFromExpirationPenalty())/2 - premiumPenalty()
-//
-// This of course, is a big deviation from initial thinking...
-// Not sure of least dumb way to convert this to list of N paths.
--- destiny/destiny_test.go
+++ destiny/destiny_test.go
@@ -1,181 +1,47 @@
 package destiny
 
 import (
-	"github.com/eliwjones/thebox/util"
-	"github.com/eliwjones/thebox/util/funcs"
 	"github.com/eliwjones/thebox/util/structs"
 
 	"testing"
-	"time"
 )
 
-func Test_Destiny_New(t *testing.T) {
-	d := New(1 * 24 * 60 * 60 * 1000)
+func dummyEdges() []structs.Maximum {
+	edges := []structs.Maximum{}
+	edges = append(edges, structs.Maximum{Underlying: "GOOG", MaximumBid: 1100, OptionAsk: 500})
+	edges = append(edges, structs.Maximum{Underlying: "GOOG", MaximumBid: 500, OptionAsk: 500})
+	edges = append(edges, structs.Maximum{Underlying: "AAPL", MaximumBid: 1000, OptionAsk: 500})
+	edges = append(edges, structs.Maximum{Underlying: "BABA", MaximumBid: 1000, OptionAsk: 500})
 
-	if len(d.paths) != 0 {
-		t.Errorf("Expected Len: 0, Got: %d!", len(d.paths))
+	return edges
+}
+
+func Test_Destiny_filterEdgesByMultiplier(t *testing.T) {
+	multiplier := 2
+	edges := filterEdgesByMultiplier(dummyEdges(), multiplier)
+
+	if len(edges) == len(dummyEdges()) {
+		t.Errorf("Expected some edges to be filtered out.")
+	}
+
+	for _, edge := range edges {
+		if edge.MaximumBid/edge.OptionAsk < multiplier {
+			t.Errorf("Expecting MaximumBid/OptionAsk < %d, Got: %d", multiplier, edge.MaximumBid/edge.OptionAsk)
+		}
 	}
 }
 
-func Test_Destiny_Get(t *testing.T) {
-	d := New(1 * 24 * 60 * 60 * 1000)
+func Test_Destiny_filterEdgesByUnderlying(t *testing.T) {
+	underlying := "GOOG"
+	edges := filterEdgesByUnderlying(dummyEdges(), underlying)
 
-	if len(d.paths) != 0 {
-		t.Errorf("Expected Len: 0, Got: %d!", len(d.paths))
-	}
-	_, err := d.Get()
-	if err == nil {
-		t.Errorf("Should have received error.  There are no paths.")
+	if len(edges) == len(dummyEdges()) {
+		t.Errorf("Expected some edges to be filtered out.")
 	}
 
-	dest := structs.Destination{Symbol: "GOOG", Type: util.STOCK}
-	path := structs.Path{Destination: dest, Timestamp: funcs.MS(funcs.Now())}
-	d.paths = append(d.paths, path)
-	dp, _ := d.Get()
-	if dp != path {
-		t.Errorf("Expected: %+v, Got: %+v!", path, dp)
-	}
-}
-
-func Test_Destiny_Put(t *testing.T) {
-	d := New(1 * 24 * 60 * 60 * 1000)
-
-	dest := structs.Destination{Symbol: "GOOG", Type: util.STOCK}
-	path := structs.Path{Destination: dest, Timestamp: funcs.MS(funcs.Now())}
-	d.Put(path, true)
-	if len(d.paths) != 1 {
-		t.Errorf("Expected Len: 1, Got: %d!", len(d.paths))
-	}
-	if d.paths[0] != path {
-		t.Errorf("Expected: %+v, Got: %+v!", path, d.paths[0])
-	}
-
-	dest = structs.Destination{Symbol: "AAPL", Type: util.STOCK}
-	path = structs.Path{Destination: dest, Timestamp: funcs.MS(funcs.Now())}
-	d.Put(path, true)
-	if len(d.paths) != 2 {
-		t.Errorf("Expected Len: 2, Got: %d!", len(d.paths))
-	}
-	if d.paths[1] != path {
-		t.Errorf("Expected: %+v, Got: %+v!", path, d.paths[1])
-	}
-
-	// Verify cannot add empty path.
-	length := len(d.paths)
-	d.Put(structs.Path{}, true)
-	if len(d.paths) != length {
-		t.Errorf("Empty Destination should not have been added!")
-	}
-}
-
-func Test_Destiny_Decay(t *testing.T) {
-	d := New(1 * 24 * 60 * 60 * 1000)
-
-	now := funcs.MS(funcs.Now())
-	dest := structs.Destination{Symbol: "AAPL", Type: util.STOCK}
-	path := structs.Path{Destination: dest, Timestamp: now - d.maxage - 10}
-	d.paths = append(d.paths, path)
-
-	// Same as with money.ReAllot() testing, this is really just the wrapper.
-	// We are testing the underlying decay() func.
-	d.Decay()
-	if len(d.paths) != 0 {
-		t.Errorf("Expected Len: 0, Got: %d!", len(d.paths))
-	}
-}
-
-func Test_Destiny_Processor_Allotment(t *testing.T) {
-	d := New(1 * 24 * 60 * 60 * 1000)
-	tc := make(chan interface{}, 10)
-	d.dispatcher.Subscribe("protoorder", "trader", tc, true)
-
-	p := structs.Path{LimitClose: 1, LimitOpen: 2, Timestamp: 3}
-	d.Put(p, true)
-
-	a := structs.Allotment{Amount: 100}
-	reply := make(chan interface{})
-	d.amIn <- structs.AllotmentMessage{Allotment: a, Reply: reply}
-
-	response := <-reply
-	if !response.(bool) {
-		t.Errorf("Should have received 'true'")
-	}
-	if len(tc) != 1 {
-		t.Errorf("Expected: 1, Got: %d!", len(tc))
-	}
-	po := <-tc
-	if (po != structs.ProtoOrder{Allotment: a, Path: p}) {
-		t.Errorf("Expected: %+v, Got: %+v!", structs.ProtoOrder{Allotment: a, Path: p}, po)
-	}
-
-	// Verify kickback of Allotment when no Paths.
-	d = New(1 * 24 * 60 * 60 * 1000)
-	d.amIn <- structs.AllotmentMessage{Allotment: a, Reply: reply}
-	response = <-reply
-	if response != a {
-		t.Errorf("No Path. Expected Allotment: %+v, Got: %+v", a, response)
-	}
-}
-
-func Test_Destiny_Processor_Delta(t *testing.T) {
-	// Currently using dummy heuristic so just test that.
-	d := New(1 * 24 * 60 * 60 * 1000)
-	pathLen := len(d.paths)
-
-	delta := structs.Delta{}
-	delta.Path = structs.Path{LimitClose: 1, LimitOpen: 2, Timestamp: 3}
-	delta.Percent = 99
-
-	d.dIn <- delta
-	for len(d.dIn) > 0 {
-		time.Sleep(1 * time.Millisecond)
-	}
-	if pathLen != len(d.paths) {
-		t.Errorf("Expected: %d, Got: %d!", pathLen, len(d.paths))
-	}
-
-	// Passing dummy heuristic threshold, should see extra path.
-	delta.Percent = 101
-
-	d.dIn <- delta
-	for len(d.dIn) > 0 {
-		time.Sleep(1 * time.Millisecond)
-	}
-	if pathLen+1 != len(d.paths) {
-		t.Errorf("Expected: %d, Got: %d!", pathLen+1, len(d.paths))
-	}
-}
-
-func Test_Destiny_Penalties(t *testing.T) {
-	k := float64(1)
-
-	p := distanceFromStrikePenalty(120, 120, 95, k)
-	if p != 1 {
-		t.Errorf("Distance Penalty - Expected: 1, Got: %4f", p)
-	}
-
-	p = distanceFromStrikePenalty(95, 120, 95, k)
-	if p != 0 {
-		t.Errorf("Distance Penalty - Expected: 0, Got: %4f", p)
-	}
-
-	p = distanceFromExpirationPenalty(int64(0), int64(5*24*60*60), k)
-	if p != 1 {
-		t.Errorf("Expiration Penalty - Expected: 1, Got: %4f", p)
-	}
-
-	p = distanceFromExpirationPenalty(int64(0), int64(1*24*60*60), k)
-	if p != 0 {
-		t.Errorf("Expiration Penalty - Expected: 0, Got: %4f", p)
-	}
-
-	p = premiumPenalty(300, 300, 30, k)
-	if p != 1 {
-		t.Errorf("Premium Penalty - Expected: 1, Got: %4f", p)
-	}
-
-	p = premiumPenalty(30, 300, 30, k)
-	if p != 0 {
-		t.Errorf("Premium Penalty - Expected: 0, Got: %4f", p)
+	for _, edge := range edges {
+		if edge.Underlying != underlying {
+			t.Errorf("Expected: %s, Got: %s", underlying, edge.Underlying)
+		}
 	}
 }
